daa471f059a47655a9f7586c4cd0fc04b268f5fb
pablosolisdalencon <pablo.solis.dalencon@gmail.com>
2025-05-02 17:43:56 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,15 +14,18 @@ function Navbar(){
         <div className="nav-container">
             <a href="/" className="nav-logo"><img src="logo.png"/></a>
             <div className="nav-links">
-                <a href="/#nosotros" className="nav-link">Nosotros</a>
-                <a href="/#catalogo" className="nav-link">Catálogo</a>
-                <a href="/#contacto" className="nav-link">Contacto</a>
+                
                 {session?.user ? (
                     <a href="proyectos" className="nav-button">Proyectos</a>
                 ):(
+                    <div>
+                    <a href="/#nosotros" className="nav-link">Nosotros</a>
+                <a href="/#catalogo" className="nav-link">Catálogo</a>
+                <a href="/#contacto" className="nav-link">Contacto</a>
                     <Link href="/login"><button onClick={()=> signIn()} className="bg-sky-500 nav-button">
-                    Sign In
+                    Ingresar
                 </button></Link>
+                </div>
                     
                 )}
             </div>
