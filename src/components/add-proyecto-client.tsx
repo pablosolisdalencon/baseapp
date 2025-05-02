2caf6cc3c64df4e86ff5b463c405dc42ba9a9f5f
pablosolisdalencon <pablo.solis.dalencon@gmail.com>
2025-05-02 15:07:03 -0400

    suspense in catalogo y proyectos

---
+++ src/components/add-proyecto-client.tsx
@@ -0,0 +1,167 @@
+"use client";
+import { ChangeEvent, FormEvent, useState, useEffect, useCallback } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { CldUploadWidget } from 'next-cloudinary';
+
+export default function AddProyectoClient() {
+  const { data: session, status } = useSession();
+  const [logoPreviewUrl, setLogoPreviewUrl] = useState<string | null>(null);
+  const [fondoPreviewUrl, setFondoPreviewUrl] = useState<string | null>(null);
+  const [newProyecto, setNewProyecto] = useState<{
+    [key: string]: string;
+    nombre: string;
+    descripcion: string;
+    mision: string;
+    vision: string;
+    texto: string;
+    frase: string;
+    mail: string;
+    fono: string;
+    logo: string;
+    fondo: string;
+    user: string;
+  }>({
+    nombre: "",
+    descripcion: "",
+    mision: "",
+    vision: "",
+    texto: "",
+    frase: "",
+    mail: "",
+    fono: "",
+    logo: "",
+    fondo: "",
+    user: session?.user?.email || "",
+  });
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session?.user?.email) {
+      setNewProyecto((prevProyecto) => ({
+        ...prevProyecto,
+        user: session.user?.email as string ?? "",
+      }));
+    }
+  }, [session]);
+
+ 
+
+  const createProyecto = async () => {
+    const res = await fetch('api/proyecto', {
+      method: "POST",
+      body: JSON.stringify(newProyecto),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    router.push('proyectos');
+    console.log(data);
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setNewProyecto({ ...newProyecto, [e.target.name]: e.target.value });
+    console.log(newProyecto);
+  };
+
+
+  const handleLogoUpload = (result: any, widget: any) => {
+      if (result && result.info && result.info.secure_url) {
+        setNewProyecto((prevData) => ({
+          ...prevData!,
+          logo: result.info.secure_url,
+        }));
+        setLogoPreviewUrl(result.info.secure_url);
+        widget.close();
+      }
+    };
+
+    const handleFondoUpload = (result: any, widget: any) => {
+      if (result && result.info && result.info.secure_url) {
+        setNewProyecto((prevData) => ({
+          ...prevData!,
+          fondo: result.info.secure_url,
+        }));
+        setFondoPreviewUrl(result.info.secure_url);
+        widget.close();
+      }
+    };
+
+
+
+  if (status === "loading") {
+    return <p>Cargando sesión...</p>;
+  }
+
+  return (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <h1>Nuevo Proyecto</h1>
+        <p>
+          ¡Estamos listos para crear tus productos digitales! 🛠️ Para empezar, necesitamos conocer a fondo tu emprendimiento. Completa este formulario con toda la información relevante. ¡Esta será la base para construir herramientas digitales poderosas para tu negocio!
+        </p>
+
+        <input onChange={handleChange} name="nombre" type="text" placeholder="Nombre del Proyecto" />
+        <textarea onChange={handleChange} name="descripcion" placeholder="Descripción breve"></textarea>
+        <textarea onChange={handleChange} name="mision" placeholder="Mision"></textarea>
+        <textarea onChange={handleChange} name="vision" placeholder="Vision"></textarea>
+
+        <textarea onChange={handleChange} name="texto" placeholder="Texto Atractivo Principal"></textarea>
+        <input onChange={handleChange} name="frase" type="text" placeholder="Frase Corta Descriptiva" />
+        <input onChange={handleChange} name="mail" type="email" placeholder="Correo Electrónico de Contacto" />
+        <input onChange={handleChange} name="fono" type="text" placeholder="Número de Teléfono" />
+      </form>
+      <CldUploadWidget uploadPreset="ewavepack" onSuccess={handleLogoUpload}>
+            {({ open }) => {
+              const handleOpenLogoWidget = useCallback(() => {
+                open();
+              }, [open]);
+              return (
+                <button className="upload-button" name="logo" onClick={handleOpenLogoWidget}>
+                  Carga la imagen del Logotipo
+                </button>
+              );
+            }}
+          </CldUploadWidget>
+          {logoPreviewUrl && (
+            <div>
+              <p>Vista previa del Logo:</p>
+              <img src={logoPreviewUrl} alt="Vista previa del logo" style={{ maxWidth: '100px', maxHeight: '100px' }} />
+            </div>
+          )}
+          <CldUploadWidget uploadPreset="ewavepack" onSuccess={handleFondoUpload}>
+            {({ open }) => {
+              const handleOpenFondoWidget = useCallback(() => {
+                open();
+              }, [open]);
+              return (
+                <button className="upload-button" name="fondo" onClick={handleOpenFondoWidget}>
+                  Carga el fondo de la eWebApp
+                </button>
+              );
+            }}
+          </CldUploadWidget>
+          {fondoPreviewUrl && (
+            <div>
+              <p>Vista previa del Fondo:</p>
+              <img src={fondoPreviewUrl} alt="Vista previa del fondo" style={{ maxWidth: '100px', maxHeight: '100px' }} />
+            </div>
+          )}
+          <hr />
+          <div className="btncancelar">
+            <a href="proyectos"><input type="button"  className="boton-cancelar" value="Cancelar" /></a>
+          </div>
+          <div className="btnfinalizar">
+            <input type="button" onClick={createProyecto} className="boton-crear-proyecto" value="Guardar" />
+          </div>
+        </div>
+  );
+}
---
+++ src/components/catalogo-client.tsx
@@ -0,0 +1,175 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+
+interface DataType {
+    id: string;
+    nombre: string;
+    
+}
+interface ItemType {
+    _id: string;
+    nombre: string;
+    id_proyecto: string;
+    data: ItemType[];
+    foto: string;
+    tipo: string;
+}
+
+
+export default  function CatalogoClient(){
+    const [dataList,setDataList] = useState<ItemType[] | null>(null);
+    const [isLoadingC, setIsLoadingC] = useState(true);
+    const [errorC, setErrorC] = useState<string | null>(null);
+
+    const [data,setData] = useState<DataType | null>(null);
+    const [isLoadingP, setIsLoadingP] = useState(true);
+    const [errorP, setErrorP] = useState<string | null>(null);
+
+    const [idProyecto, setIdProyecto] = useState<string | null>(null);
+   
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    
+    useEffect( () => {
+        router.refresh();
+        
+        const id = searchParams.get('id')
+        if (id){
+            console.log(id);
+            setIdProyecto(id);
+            //traer ficha proyecto
+            const getFichaProyecto = async () => {
+                setIsLoadingP(true);
+                setErrorP(null);
+
+                try {
+                    const response = await fetch('api/proyecto/'+id);
+                    if(!response.ok){
+                        throw new Error(`<br>HTTP error! in Proyecto status: ${response.status}`);
+                    }
+
+                    const jsonData: DataType = await response.json();
+                    setData(jsonData);
+                
+                }catch (e: any){
+                    setErrorP('<br>Error al cargar Proyecto['+id+']:'+e.message);
+                    setData(null);
+                } finally {
+                    setIsLoadingP(false);
+                }
+            };
+       
+
+            // traer todo el catalogo del proyecto
+
+            const getCatalogo = async () => {
+                setIsLoadingC(true);
+                setErrorC(null);
+
+                try {
+                    const response = await fetch('api/item/?p='+id);
+                    if(!response.ok){
+                        throw new Error(`<br>HTTP error! in Catalogo status: ${response.status}`);
+                    }
+                    const jsonData: ItemType = await response.json();
+                    setDataList(jsonData.data);
+                
+                }catch (e: any){
+                    setErrorC('<br>Error al cargar Catalogo :'+e.message);
+                    setDataList(null);
+                } finally {
+                    setIsLoadingC(false);
+                }
+            };
+        
+         
+            if( !getFichaProyecto()){
+                setErrorC('<br>Algo paso ejecutando la funcion getFichaProyecto(); linea 84');
+            }else{
+                getCatalogo();
+            }   
+        }
+    },[router]);
+
+    // ELIMINAR 
+
+    const eliminar = async (id:string) => {
+        const res = await fetch('api/item/'+id, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await res.json()
+        if(data){
+            alert("Eliminado correctamente");
+            console.log(data)
+            router.push(`catalogo/?id=${idProyecto}`)
+        }
+        
+    }
+
+    const goEliminar = async (id:string) => {
+        await eliminar(id);
+    }
+
+    
+    
+    if(isLoadingP){
+        return <p>Cargando ficha Proyecto...</p>;
+    }
+    if(errorP){
+        return <p>Error cargando el proyecto: {errorP}</p>;
+    }
+
+    if(isLoadingC){
+        return <p>Cargando datos del Catalogo...</p>;
+    }
+    if(errorC){
+        return <p>Error cargando el catalogo: {errorC}</p>;
+    }
+    
+    const nombreProyecto=data?.nombre as string;
+
+   
+    return(
+        <div className="catalog-manager-container">
+            <h1 className="project-title">{nombreProyecto}</h1>
+            <h2 className="catalog-subtitle">Gestión del Catálogo del Proyecto</h2>
+
+            { dataList?.length as number > 0 ?  (
+                    <ul className="catalog-list">
+                          {dataList?.map((item) => (
+                                <li className="catalog-item" key={item._id}>
+                                    <div className="catalog-item-content">
+                                        <img src={item.foto} alt="Producto 1" className="product-image"/>
+                                        <div className="product-info">
+                                            <h3 className="product-name">{item.nombre}</h3>
+                                            <span className="product-type-label">{item.tipo}</span>
+                                        </div>
+                                        <div className="catalog-actions">
+                                            <Link href={`updateitem?id=${item._id}&nombreProyecto=${nombreProyecto}&idProyecto=${idProyecto}`} > <button className="button-edit" >Editar</button></Link>
+                                            <button onClick={()=>goEliminar(item._id)} className="button-delete">Eliminar</button>
+                                        </div>
+                                    </div>
+                                </li>
+                        ))}
+                    </ul>
+            ) :(
+
+                <p>sin datos </p> 
+               
+            )
+            }
+        <hr />
+            <Link href={`additem/?idProyecto=${idProyecto}`}><button className="button-add-proyecto"> + Agregar Nuevo Item al Catalogo</button></Link>
+        </div>
+    );
+}
+
+function refreshRouter() {
+    throw new Error("Function not implemented.");
+}
