ada3f457e21b1bff20f828276208883250852e08
pablosolisdalencon <pablo.solis.dalencon@gmail.com>
2025-06-22 19:09:19 -0400

    cleaning no used apis

--- src/app/api/user-tokens/route.js
+++ src/app/api/user-tokens/route.js
@@ -11,9 +11,9 @@ import { URL } from 'url';
 // En un sistema real, obtendrías el email del usuario de un token JWT o sesión.
 const getAuthenticatedUserEmail = (data) => {
    
-    console.log("getAuthenticatedUserEmail")
+    //console.log("getAuthenticatedUserEmail")
     const emailFromQuery = data.email
-    console.log(emailFromQuery)
+   // console.log(emailFromQuery)
     // En un POST/PUT, podrías obtenerlo del body o de cabeceras de autenticación.
     // Para simplificar, asumimos que viene en el query o lo hardcodeamos.
     return emailFromQuery || 'testuser@example.com';
@@ -24,25 +24,25 @@ export async function GET(request) {
     const searchParams = new URL(request.url).searchParams;
     const email = searchParams.get('e');
 
-    console.log("user TOKENS GET API say email:")
+    //console.log("user TOKENS GET API say email:")
     await connectDB();
-    console.log(email)
+    //console.log(email)
 
     try {
         
         let userTokens = await UserToken.findOne({ email:email });
-        console.log("user TOKENS GET API say tokens:")
-        console.log(userTokens.tokens)
+        //console.log("user TOKENS GET API say tokens:")
+        //console.log(userTokens.tokens)
         return NextResponse.json({ tokens: userTokens.tokens });
     } catch (error) {
-        console.error('Error fetching user tokens:', error);
+        //console.error('Error fetching user tokens:', error);
         return NextResponse.json({ message: 'Server error fetching user tokens.' }, { status: 500 });
     }
 }
 
 // PUT /api/user-tokens - Descuenta tokens del usuario (método "use")
 export async function PUT(request){
-    console.log("user TOKENS PUT API say email:")
+    //console.log("user TOKENS PUT API say email:")
 
     try{
         const data = await request.json()
@@ -67,8 +67,8 @@ export async function POST(request){
     connectDB();
     
     const data = await request.json();
-    console.log("=============   API UserTokens POST say data:!!   ==================")
-        console.log(data)
+    //console.log("=============   API UserTokens POST say data:!!   ==================")
+     //   console.log(data)
     if(data){
       //const Jdata = JSON.parse(data)
       const Jdata = data
@@ -76,8 +76,8 @@ export async function POST(request){
         const savedData = await newData.save()    
         return NextResponse.json({"message": `holas UserTokens POST: ${savedData}`});
     }   else    {
-        console.log("=============   DATA NULL !!   ==================")
-        console.log(data)
+       // console.log("=============   DATA NULL !!   ==================")
+       // console.log(data)
         return NextResponse.json({ message: "Error al rescatar la data de UserTokens en el Request" }, { status: 500 });
     }
 
