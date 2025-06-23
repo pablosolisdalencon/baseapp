754afc9379e0a330ecfb18fd76b9de0c4afe2df9
pablosolisdalencon <pablo.solis.dalencon@gmail.com>
2025-06-22 21:22:46 -0400

    Update page.tsx

--- src/app/proyectos/page.tsx
+++ src/app/proyectos/page.tsx
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import ProyectosClient from "@/components/proyectos-client";
 import { Suspense } from "react";
+import dynamic from "next/dynamic";
+const UpdateProyectoClient = dynamic(() => import("@/components/update-proyecto-client"));
 
 export default async function Proyectos() {
   const session = await getServerSession(authOptions);
@@ -15,6 +17,7 @@ export default async function Proyectos() {
     );
   }
   return(
-    <Suspense><ProyectosClient/></Suspense>
+    <Suspense fallback={<p>Cargando...</p>}>
+      <ProyectosClient/></Suspense>
   )
 }
