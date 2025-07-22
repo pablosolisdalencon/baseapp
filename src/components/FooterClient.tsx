aec872350fe53314749965c8d460674e7c4df6ba
pablosolisdalencon <pablo.solis.dalencon@gmail.com>
2025-07-22 16:00:21 -0400

    Update FooterClient.tsx

--- src/components/FooterClient.tsx
+++ src/components/FooterClient.tsx
@@ -1,9 +1,5 @@
 'use client';
-import Link from "next/link";
-import { useSession, signIn, signOut } from 'next-auth/react';
-
 function FooterClient() {
-  const { data: session } = useSession();
   
   return (
     <div className="p-4 bg-blue-950 text-amber-50 rounded-lg shadow-sm border border-gray-200 mb-4 align-middle">
